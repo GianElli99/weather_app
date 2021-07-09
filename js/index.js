7f28e6b12082b4ad8402ddeabfffea6951307317
GianElli99 <gianelli99@hotmail.com>
2021-07-09 18:10:48 -0300

    hardcoded api call in today details section

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var APIkey = 'e35b38d0fdfdbe50df60d75dfb60145e';
 var coord = {
-  lon: -71.300003,
-  lat: -41.150002,
+  lon: -60,
+  lat: -36,
 };
 var days = [
   'Sunday',
@@ -12,6 +12,56 @@ var days = [
   'Friday',
   'Saturday',
 ];
+function GetWindDirection(degress) {
+  if (degress <= 11.25 || degress >= 348.75) {
+    return 'N';
+  }
+  if (11.25 < degress && degress <= 33.75) {
+    return 'NNE';
+  }
+  if (33.75 < degress && degress <= 56.25) {
+    return 'NE';
+  }
+  if (56.25 < degress && degress <= 78.75) {
+    return 'ENE';
+  }
+  if (78.75 < degress && degress <= 101.25) {
+    return 'E';
+  }
+  if (101.25 < degress && degress <= 123.75) {
+    return 'ESE';
+  }
+  if (123.75 < degress && degress <= 146.25) {
+    return 'SE';
+  }
+  if (146.25 < degress && degress <= 168.75) {
+    return 'SSE';
+  }
+  if (168.75 < degress && degress <= 191.25) {
+    return 'S';
+  }
+  if (191.25 < degress && degress <= 213.75) {
+    return 'SSW';
+  }
+  if (213.75 < degress && degress <= 236.25) {
+    return 'SW';
+  }
+  if (236.25 < degress && degress <= 258.75) {
+    return 'WSW';
+  }
+  if (258.75 < degress && degress <= 281.25) {
+    return 'W';
+  }
+  if (281.25 < degress && degress <= 303.75) {
+    return 'WNW';
+  }
+  if (303.75 < degress && degress <= 326.25) {
+    return 'NW';
+  }
+  if (326.25 < degress && degress <= 348.75) {
+    return 'NNW';
+  }
+}
 
 var input = document.getElementById('search-input');
 var todayTemperatureValueElement = document.getElementById(
@@ -85,7 +135,48 @@ function UpdateSummary(data) {
   listItem.appendChild(itemDesc);
   todayWeatherListElement.appendChild(listItem);
 }
-function UpdateTodayDetails(data) {}
+function UpdateTodayDetails(data) {
+  var uvImgElement = document.getElementById('uv-image');
+  var uvIndex =
+    Math.round(data.current.uvi) !== 0
+      ? Math.round(data.current.uvi).toString()
+      : '1';
+  uvImgElement.src = './weather-icons/fill/all/uv-index-' + uvIndex + '.svg';
+
+  var windValuElement = document.getElementById('wind-value-number');
+  windValuElement.textContent =
+    Math.round((data.current.wind_speed / 1000) * 3600 * 10) / 10;
+  var windDirElement = document.getElementById('wind-direction');
+  windDirElement.textContent = GetWindDirection(data.current.wind_deg);
+
+  var suriseValueElement = document.getElementById('sunrise-value');
+  var sunsetValueElement = document.getElementById('sunset-value');
+  var sunriseTime = new Date(data.current.sunrise * 1000);
+  var sunsetTime = new Date(data.current.sunset * 1000);
+
+  var minutes =
+    sunriseTime.getMinutes() > 9
+      ? sunriseTime.getMinutes().toString()
+      : '0' + sunriseTime.getMinutes().toString();
+  suriseValueElement.textContent =
+    sunriseTime.getHours().toString() + ':' + minutes;
+
+  minutes =
+    sunsetTime.getMinutes() > 9
+      ? sunsetTime.getMinutes().toString()
+      : '0' + sunsetTime.getMinutes().toString();
+  sunsetValueElement.textContent =
+    sunsetTime.getHours().toString() + ':' + minutes;
+
+  var humidityValueElement = document.getElementById('humidity-value');
+  humidityValueElement.textContent = data.current.humidity;
+
+  var visibilityValueElement = document.getElementById(
+    'visibility-value-number'
+  );
+  visibilityValueElement.textContent =
+    Math.round((data.current.visibility / 1000) * 10) / 10;
+}
 function UpdateForecast(data) {
   for (let i = 1; i <= 7; i++) {
     var forecastDate = new Date(data.daily[i].dt * 1000);
